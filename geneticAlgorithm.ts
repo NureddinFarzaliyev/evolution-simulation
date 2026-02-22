0a5ebb46ca269dd55cf933eb9eeb0275a15826bc
NureddinFarzaliyev <nureddin.fa@gmail.com>
2026-02-22 10:16:11 +0400

    Cached fit & changed types
    Changed population interface from string[] to {DNA: string, fit: number}[] to cache the fitness value and avoid unnecessary recalculations

--- geneticAlgorithm.ts
+++ geneticAlgorithm.ts
@@ -1,7 +1,10 @@
 import { checkByPercentage, generateRandomInteger } from "./utils/random";
 
 // Generates a random DNA from a given set of symbols.
-function generateDNA(dnaSize: number, symbols: string) {
+
+type DNA = string;
+
+function generateDNA(dnaSize: number, symbols: string): DNA {
   let newMember = "";
   for (let i = 0; i < dnaSize; i++) {
     const randomIndex = generateRandomInteger(0, symbols.length - 1);
@@ -10,8 +13,10 @@ function generateDNA(dnaSize: number, symbols: string) {
   return newMember;
 }
 
+type Fit = number;
+
 // Finds the fitness of the DNA to the targetDNA based on correct letters and close letters in the string
-function fitness(DNA: string, targetDNA: string) {
+function fitness(DNA: DNA, targetDNA: DNA): Fit {
   let fit = 0;
   for (let i = 0; i < DNA.length; i++) {
     const diff = Math.abs(DNA.charCodeAt(i) - targetDNA.charCodeAt(i));
@@ -27,25 +32,34 @@ function fitness(DNA: string, targetDNA: string) {
   return fit;
 }
 
+interface PopulationMember {
+  DNA: DNA;
+  fit: Fit;
+}
+
+type Population = PopulationMember[];
+
 // Generates a population with random DNAs in given size
 function generatePopulation(
   dnaSize: number,
   symbols: string,
   populationSize: number,
-) {
-  const population: string[] = [];
+  fitness: (DNA: DNA) => Fit,
+): Population {
+  const population: Population = [];
   for (let i = 0; i < populationSize; i++) {
-    population.push(generateDNA(dnaSize, symbols));
+    const DNA = generateDNA(dnaSize, symbols);
+    const fit = fitness(DNA);
+    population.push({ DNA, fit });
   }
   return population;
 }
 
 // Uses Tournament method by choosing a small group from the population, and finding the most fit in that group
 function tournamentSelect(
-  population: string[],
-  fitnessFn: (DNA: string) => number,
+  population: Population,
   tournamentSize: number,
-) {
+): PopulationMember {
   let bestDNA = "";
   let bestFitness = -Infinity;
 
@@ -53,11 +67,11 @@ function tournamentSelect(
     const candidate =
       population[generateRandomInteger(0, population.length - 1)];
 
-    const candidateFitness = fitnessFn(candidate);
+    const candidateFitness = candidate.fit;
 
     if (candidateFitness > bestFitness) {
       bestFitness = candidateFitness;
-      bestDNA = candidate;
+      bestDNA = candidate.DNA;
     }
   }
 
@@ -66,7 +80,7 @@ function tournamentSelect(
 
 // Generates new child from given parents
 // Uses Uniform Crossover by inheriting a gene randomly from one of the parents.
-function generateChildDNA(a: string, b: string) {
+function generateChildDNA(a: DNA, b: DNA): DNA {
   let childDNA = "";
 
   for (let i = 0; i < a.length; i++) {
@@ -82,7 +96,7 @@ function generateChildDNA(a: string, b: string) {
 
 // Mutates a DNA
 // Randomly chooses a gene based on mutation rate and changes it to another random one
-function mutateChildDNA(DNA: string, mutationRate: number, symbols: string) {
+function mutateChildDNA(DNA: DNA, mutationRate: number, symbols: string) {
   let mutatedDNA = "";
 
   for (let i = 0; i < DNA.length; i++) {
@@ -97,7 +111,7 @@ function mutateChildDNA(DNA: string, mutationRate: number, symbols: string) {
 }
 
 interface EvolveParams {
-  targetDNA: string;
+  targetDNA: DNA;
   symbols: string;
   populationSize: number;
   mutationRate: number;
@@ -118,38 +132,35 @@ function evolve(params: EvolveParams) {
     targetDNA.length,
     symbols,
     populationSize,
+    (DNA) => fitness(DNA, targetDNA),
   );
 
   // Track global best dna and fitness
-  let globalBestDNA = "";
-  let globalBestFitness = -Infinity;
+  let globalBest: PopulationMember = { DNA: "", fit: -Infinity };
 
   // Track fitness history to adapt mutation
-  let fitnessHistory: number[] = [];
+  let fitnessHistory: Fit[] = [];
   const fitnessHistoryMaxLength = 5;
 
   // Start evolving over generations
   for (let generation = 0; generation < maximumGenerations; generation++) {
     // Stop if perfect match is found
-    if (globalBestDNA === targetDNA) break;
+    if (globalBest.DNA === targetDNA) break;
 
     // Track generation's best dna and fitness
-    let generationBestDNA = "";
-    let generationBestFitness = -Infinity;
+    let generationBest: PopulationMember = { DNA: "", fit: -Infinity };
 
     // Evaluate best for each generation
-    for (const DNA of population) {
-      const fit = fitness(DNA, targetDNA);
-
+    for (const member of population) {
       // Update best in this generation
-      if (fit > generationBestFitness) {
-        generationBestFitness = fit;
-        generationBestDNA = DNA;
+      if (member.fit > generationBest.fit) {
+        generationBest.fit = member.fit;
+        generationBest.DNA = member.DNA;
       }
     }
 
     // Add generation's best fitness to history
-    fitnessHistory.push(generationBestFitness);
+    fitnessHistory.push(generationBest.fit);
     if (fitnessHistory.length > 5) {
       fitnessHistory = fitnessHistory.slice(
         fitnessHistory.length - fitnessHistoryMaxLength,
@@ -157,30 +168,22 @@ function evolve(params: EvolveParams) {
     }
 
     // Update globals
-    if (generationBestFitness > globalBestFitness) {
-      globalBestFitness = generationBestFitness;
-      globalBestDNA = generationBestDNA;
+    if (generationBest.fit > globalBest.fit) {
+      globalBest.fit = generationBest.fit;
+      globalBest.DNA = generationBest.DNA;
     }
 
     console.log(
-      ` ${generation} | ${globalBestDNA} | ${globalBestFitness.toFixed(2)}`,
+      ` ${generation} | ${globalBest.DNA} | ${globalBest.fit.toFixed(2)}`,
     );
 
-    const newPopulation: string[] = [];
+    const newPopulation: Population = [];
 
     // Generate child dnas from fittest population using tournament and assign it as new population
     for (let i = 0; i < populationSize - 2; i++) {
       // Select two parents from the population using tournament method
-      const parent1 = tournamentSelect(
-        population,
-        (DNA) => fitness(DNA, targetDNA),
-        5,
-      ).DNA;
-      const parent2 = tournamentSelect(
-        population,
-        (DNA) => fitness(DNA, targetDNA),
-        5,
-      ).DNA;
+      const parent1 = tournamentSelect(population, 5).DNA;
+      const parent2 = tournamentSelect(population, 5).DNA;
 
       // Generate child based on chosen parents
       const childDNA = generateChildDNA(parent1, parent2);
@@ -191,11 +194,11 @@ function evolve(params: EvolveParams) {
       const stagnationThreshold = 0.001;
       const isStagnated =
         fitnessHistory.length === fitnessHistoryMaxLength &&
-          fitnessHistory[4] - fitnessHistory[0] < stagnationThreshold
+        fitnessHistory[4] - fitnessHistory[0] < stagnationThreshold
           ? 0.01
           : 0;
-
       const stagnationBonus = isStagnated ? mutationRate * 0.5 : 0;
+
       const currentMutationRate =
         mutationRate * (1 - generation / maximumGenerations) + stagnationBonus;
       const mutatedChildDNA = mutateChildDNA(
@@ -204,13 +207,15 @@ function evolve(params: EvolveParams) {
         symbols,
       );
 
+      const mutatedChildFit = fitness(mutatedChildDNA, targetDNA);
+
       // Push the child to new population
-      newPopulation.push(mutatedChildDNA);
+      newPopulation.push({ DNA: mutatedChildDNA, fit: mutatedChildFit });
     }
 
     // Elitism - global best and generation best
-    newPopulation.push(globalBestDNA);
-    newPopulation.push(generationBestDNA);
+    newPopulation.push({ ...globalBest });
+    newPopulation.push({ ...generationBest });
 
     // Replace old population for new generation iteration
     population = newPopulation;
